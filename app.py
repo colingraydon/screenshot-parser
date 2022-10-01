584fa203bf9fd97faea827f62f2144448af3a7d5
Colin Graydon <colingraydon@gmail.com>
2022-10-01 18:09:42 -0400

    screenshot working, monitor selected

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import NW, filedialog, Text
 import numpy as np
 import pyautogui
-import pygetwindow
 import pytesseract
 from PIL import Image
 import cv2
@@ -13,20 +12,32 @@ root = tk.Tk()
 
 
 #can change background color here
-canvas = tk.Canvas(root, height=600, width = 800, bg="#263D42")
+canvas = tk.Canvas(root, height=600, width = 800, bg = "#2A403D")
 canvas.pack()
 
 
 def take_screenshot():
 
     screenshot = pyautogui.screenshot()
-    titles = pygetwindow.getAllTitles()
-    window = pygetwindow.getWindowsWithTitle("Dota2")[0]
-    left, top = window.topleft
-    right, bottom = window.bottomright
+
+    #setting points for cropping timer
+    width, height = screenshot.size
+    timer_left = (width / 2) - 50
+    timer_right = (width / 2) + 50
+    timer_top = 50
+    timer_bot = 150
+
+    screenshot_cropped = screenshot.crop((timer_left, timer_top, timer_right, timer_bot))
+
+    #these lines will save the screenshot if needed later
+    # save_path = filedialog.asksaveasfilename()
+    # screenshot_cropped.save(save_path+"screenshottest.png")
+
+    tk_image = tk.PhotoImage(screenshot_cropped)
+    canvas.create_image(10,10, image=tk_image, anchor=tk.NW)
 
 
-roshTimer = tk.Button(root, text="Rosh Timer", command = take_screenshot, padx=10, pady=5,  fg="white", bg="#263D42")
+roshTimer = tk.Button(root, text="Rosh Timer", command = take_screenshot, padx=10, pady=5,  fg="white")
 roshTimer.pack()
 
 root.mainloop()
