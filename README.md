2b16604764d42bf913754c7fa809e4eb689bd2a7
Colin Graydon <colingraydon@gmail.com>
2022-10-02 14:37:20 -0400

    parsed text, now working on frontend

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # screenshot-parser
+
+This app is intended to be a tool for players of the game Dota2. There is an in-game timer and it can be important to remember exact times in game. 
+
+Instead of leaving it to memory, this app has a GUI which will allow the user to screenshot the timer, run optical character recognition, and then show the output on a gui.
+
+While this program supports multiple monitors, it will only currently work if Dota2.exe is shown on the primary monitor.
--- app.py
+++ app.py
@@ -1,31 +1,32 @@
 import tkinter as tk
-from tkinter import NW, filedialog, Text
-import numpy as np
+from tkinter import NW, filedialog, Text, Label
 import pyautogui
 import pytesseract
-from PIL import Image
-import cv2
-import os
-from mss import mss
+
 
 root = tk.Tk()
 
 
 #can change background color here
-canvas = tk.Canvas(root, height=600, width = 800, bg = "#2A403D")
+canvas = tk.Canvas(root, height=400, width = 400, bg = "#222222")
 canvas.pack()
 
+# #creates the grid for times
+# lbl = Label(root, text="hello world")
+# lbl.grid(row=0, column=0)
+# lbl.pack()
 
 def take_screenshot():
 
     screenshot = pyautogui.screenshot()
 
     #setting points for cropping timer
+    #this should work on any size monitor
     width, height = screenshot.size
     timer_left = (width / 2) - 50
     timer_right = (width / 2) + 50
     timer_top = 50
-    timer_bot = 150
+    timer_bot = 350
 
     screenshot_cropped = screenshot.crop((timer_left, timer_top, timer_right, timer_bot))
 
@@ -33,11 +34,35 @@ def take_screenshot():
     # save_path = filedialog.asksaveasfilename()
     # screenshot_cropped.save(save_path+"screenshottest.png")
 
-    tk_image = tk.PhotoImage(screenshot_cropped)
-    canvas.create_image(10,10, image=tk_image, anchor=tk.NW)
+    #these lines would put the screenshot onto the canvas
+    # tk_image = tk.PhotoImage(screenshot_cropped)
+    # canvas.create_image(10,10, image=tk_image, anchor=tk.NW)
 
+    text = pytesseract.image_to_string(screenshot_cropped)
+    print(text)
+    #return text
 
-roshTimer = tk.Button(root, text="Rosh Timer", command = take_screenshot, padx=10, pady=5,  fg="white")
-roshTimer.pack()
+def roshan():
+
+    s = take_screenshot
+    res = s + "Roshan"
+    add_text(res)
+
+def buyback():
+
+    s = take_screenshot
+    res = s + "Buyback"
+    add_text(res)
+
+def add_text(s):
+
+    canvas.create_text(text=s, fill="white")
+
+roshTimer = tk.Button(root, text="Rosh", command = take_screenshot, padx=10, pady=5 , fg="#222222")
+roshTimer.pack(side = "left", anchor="e", expand=True)
+buyback = tk.Button(root, text="Buyback", command = take_screenshot, padx=10, pady=5 , fg="#222222")
+buyback.pack(side="left", anchor="w", expand=True)
+clear = tk.Button(root, text="Clear", command = take_screenshot, padx=10, pady=5 , fg="#222222")
+clear.pack(side="left", anchor="w", expand=True)
 
 root.mainloop()
